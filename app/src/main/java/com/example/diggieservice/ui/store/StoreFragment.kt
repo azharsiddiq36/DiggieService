682fef186076a2f3945397c5a35b7906e9af5f11
oneclickdemocracy <developer@oneclickdemocracy.com>
2021-07-23 15:21:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/diggieservice/ui/store/StoreFragment.kt
@@ -0,0 +1,45 @@
+package com.example.diggieservice.ui.store
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.diggieservice.databinding.FragmentStoreBinding
+import com.example.diggieservice.ui.notifications.HistoryViewModel
+
+class StoreFragment : Fragment() {
+
+    private lateinit var historyViewModel: HistoryViewModel
+    private var _binding: FragmentStoreBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        historyViewModel =
+            ViewModelProvider(this).get(HistoryViewModel::class.java)
+
+        _binding = FragmentStoreBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        historyViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
